77ea7b4aa25d273989f2ab2a8fb94e217a4476c6
Esdras S. Santos <rejesdras@gmail.com>
2026-04-29 20:37:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,17 @@ async function carregarRepos() {
 }
 
 carregarRepos();
+
+// animação ao rolar
+const elements = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        const screen = window.innerHeight;
+
+        if (position < screen - 100) {
+            el.classList.add("show");
+        }
+    });
+});
